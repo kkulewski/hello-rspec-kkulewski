8736e46de0eda872ce34da53828967c7696ec136
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-10-28 22:17:38 +0200

    Add quiz 1 task 1 test

---
+++ CodeQuizzes/spec/q1_1_test.rb
@@ -0,0 +1,14 @@
+require '../lib/q1_1'
+
+RSpec.describe '../lib/q1_1' do
+  describe '#word_count' do
+    it 'returns number of words in a string' do
+      string = "laughter, it's free"
+      expect(word_count(string)).to eq 3
+    end
+
+    it 'returns 0 for empty string' do
+      expect(word_count('')).to eq 0
+    end
+  end
+end
